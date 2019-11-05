da487ffc98038d0dd44e7a2b5dcbc1e274cbe7e4
Mary Jane <35101823+EchoJane1943@users.noreply.github.com>
2019-11-05 13:29:18 +0800

    Update dpmm_cgs.R

--- DPMMwithCGS/dpmm_cgs.R
+++ DPMMwithCGS/dpmm_cgs.R
@@ -1,18 +1,20 @@
-# 生成多维高斯混合分布数据
+## generate data which follow GMM
+
 set.seed(2019)
-gene_data <- function(n,p){
+
+gene_data <- function(n,p,K){
   # n: length of data
   # p: dimention of x
   # rho: Covariance matrix correlation
-  rho       <- runif(p,0,1)
-  w         <- DIRECT::rDirichlet(1,rep(1,p)) # weights of every distribution
+  rho       <- runif(K,0,1)
+  w         <- DIRECT::rDirichlet(1,rep(1,K)) # weights of every distribution
   x         <- data.frame()
   means     <- list()
   sigmas    <- list()
-  for (i in (1:p)) {
+  for (i in (1:K)) {
     n_samples <- ceiling(n*w[i])
-    mean      <- rnorm(p,0,1)
-    sigma_mat <- function(p,rho){diag(1-rho,p,p)+rho}
+    mean      <- rnorm(p,0,1)+10*p
+    sigma_mat <- function(p,rho){diag(1-rho,p)+rho}
     sigma     <- sigma_mat(p,rho[i])
     temp      <- MASS::mvrnorm(n_samples, mean, sigma)
     x         <- rbind(x,temp)
@@ -22,89 +24,105 @@ gene_data <- function(n,p){
   return(list(x = x[1:n,],w = w,means = means,sigma = sigmas,rho = rho))
 }
 
-result <- gene_data(50,5)
+result <- gene_data(100,2,3)
 
+## GSDMM
 
-# DPMM and CGS
-
-get_n_k_i <- function(arr,i,k){
-  mat <-  as.data.frame(table(arr[1:i]),names=c("Var1","Freq"))
-  res <- ifelse(i<1,0,mat$Freq[mat$Var1==k])
-  return(res)
+# get the number of data points in k cluster N_{-i,k}
+get_n_i_k <- function(arr,i,k){
+  if(i<1){return(0)}
+  else{
+    mat <- as.data.frame(table(arr[-i]),names=c("Var1","Freq"))
+    return(mat$Freq[mat$Var1==k])
+  }
 }
 
-crp_cgs <- function(x,T = 1,alpha = 1){
+# define Z function
+Z_func <- function(D,lambda,S,nu){
+  p1 <- 2^((nu+1)*D/2)
+  p2 <- pi^(D*(D+1)/4)
+  p3 <- lambda^(-D/2)
+  p4 <- det(S)^(-nu/2)
+  p5 <- prod(unlist(lapply((nu+1-c(1:D))/2,gamma)))
+  return(prod(p1,p2,p3,p4,p5))
+}
+
+crp_cgs <- function(x,T = 100,alpha = 10,K = 5){
   
   # xi: data
   # zi: clusters
   
-  Phi <- function(nk,i.=i,Phi0.=Phi0,lambda.=lambda,mu0.=mu0,x_bar.=x_bar,x.=x){
-    munk <- (lambda.*mu0.+nk*x_bar.)/(lambda.+nk)
-    res  <- Phi0.+lambda.*mu0.%*%t(mu0.)-(lambda.+nk)*(munk)%*%t(munk)
-    for(j in 1:i){res = res+as.matrix(t(x.[i,]))%*%as.matrix(x.[i,])}
+  # caculate Phi_n_k
+  Phi <- function(nk){
+    x_bar = colMeans(x[ns_k,])
+    munk <- (lambda*mu0+nk*x_bar)/(lambda+nk)
+    res  <- Phi0+lambda*(mu0%*%t(mu0))-(lambda+nk)*(munk%*%t(munk))
+    for(j in ns_k){res = res+as.matrix(t(x[ns_k,]))%*%as.matrix(x[ns_k,])}
     return(res)
   }
   
+  # initial values
   N = dim(x)[1]
   D = dim(x)[2]
-  K = 5
-  Z      <- sample(c(1:K),size = N,replace = TRUE)
-  mat    <-  as.data.frame(table(Z),names=c("Var1","Freq"))
-  lambda <- 10
-  nu     <- 10
-  mu0    <- rep(10,D)
-  Phi0   <- diag(10,D)
+  
+  lambda <- 0.1
+  nu     <- -1
+  mu0    <- colMeans(x)
+  Phi0   <- diag(0.1,D)
+
+  
+  # inicialize z_i for y_i to a random table
+  Z <- sample(c(1:K),size = N,replace = TRUE) 
+
   for (t in 1:T) {
-    print(t)
+    cat("t=",t,"\n")
     for (i in 1:N) {
-      
       cat("i=",i,"\n")
-      p1 = c()
-      p2 = c()
-      p3 = c()
-      x_bar = colMeans(x[1:i,])
-      
+      # inicialize vector p 
+      p1 = c() # prior
+      p2 = c() # likelihood
+      p3 = c() # post
+      # update K & Z 
+      K = length(unique(Z))
+     
       for (k in 1:K) {
-        
+        ns_k = which(Z==k)
+        x_bar = colMeans(x[ns_k,])
         # p(z_i=k|z_-i,alpha) == p1
-        p1tem = get_n_k_i(Z,i-1,k)/(i-1+alpha)
-        p1=c(p1,p1tem)
-        cat("p1=",p1,"\n")
+        p1tem = get_n_i_k(Z,i,k)/(N-1+alpha)
+        p1=c(p1,-log(p1tem))
         
         # p(x_i|x_-i,k,Phi) == p2
-        
-        nk = get_n_k_i(Z,i,k)
-        
-        p2tem = pi^(-D/2)*((lambda+nk)/(lambda+nk-1))^(-D/2)*
-          det(Phi(nk))^(-(nu+nk)/2)/det(Phi(nk-1))^(-(nu+nk-1)/2)*
-          gamma((nu+nk)/2)/gamma((nu+nk-D)/2)
-        p2 = c(p2,p2tem)
-        cat("p2=",p2,"\n")
+        nk = get_n_i_k(Z,length(Z)+1,k)
+        p2tem = (2*pi)^(-D/2)*(Z_func(D,lambda+nk,Phi(nk),nu+nk)/Z_func(D,lambda+nk-1,Phi(nk-1),nu+nk-1))
+        p2 = c(p2,-log(p2tem))
+        #cat("p2=",p2,"\n")
         
         # p(z_i=k|z_-i,x_i,alpha,Phi)== p3
-        p3tem = p1tem * p2tem
+        p3tem = p1[k] + p2[k]
         p3 = c(p3,p3tem)
-        cat("p3=",p3,"\n")
-        
+        # cat("p3=",p3,"\n")
       }
+      p1tem =alpha/(N-1+alpha)
+      p1 = c(p1,-log(p1tem))
       
-      p1tem =alpha/(i-1+alpha)
-      p1 = c(p1,log(p1tem))
+      p2tem = (2*pi)^(-D/2)*(Z_func(D,lambda+1,Phi(1),nu+1)/Z_func(D,lambda,Phi0,nu))
+      p2 = c(p2,-log(p2tem))
       
-      p2tem = pi^(-D/2)*((lambda+1)/(lambda+1-1))^(-D/2)*
-        det(Phi(1))^(-(nu+1)/2)/det(Phi(1-1))^(-(nu+1-1)/2)*
-        gamma((nu+1)/2)/gamma((nu+1-D)/2)
-      p2 = c(p2,log(p2tem))
-      
-      p3tem = p1tem * p2tem
+      p3tem = p1[K+1] + p2[K+1]
       p3 = c(p3,p3tem)
     }
+    
+    p3[is.finite(p3)== FALSE | is.na(p3)== TRUE] = 0
     print(p3)
-    print(K)
-    Z <- sample(1:(K+1),size = N,replace = TRUE,prob = p3)
-    K = length(unique(Z))
+    Z[i] <- sample(1:(K+1),size = 1,prob = p3)
+    
+    print(Z)
+    print(length(unique(Z)))
+    
   }
   return(Z)
 }
 
-res <- crp_cgs(result$x)
+res <- crp_cgs(result$x,T = 100)
+
